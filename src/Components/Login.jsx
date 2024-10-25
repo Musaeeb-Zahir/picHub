87d79be35fcc4ece37be34d5c357c94deb09a995
musaeeb-zahir <musaeebzahir@gmail.com>
2024-10-25 23:25:12 +0500

    fix anather error

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import "../styles/Auth.css";
+import "../Styles/Auth.css";
 import appStore from "../redux/store";
 import { Navigate, useNavigate } from "react-router-dom";
 import { setLoginUser } from "../redux/slices/loginFormSlice";
